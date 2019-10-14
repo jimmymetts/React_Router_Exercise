5c37e982607243a70ad1451f100acbf505456298
jimmymetts <jimmymetts@bellsouth.net>
2019-10-14 14:23:09 -0500

    updated shop.js

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function Shop() {
+
+    useEffect(() => {
+        fetchMessage();
+    },[]);
+
+    const [messages, setMessage] = useState([]);
+
+    const fetchMessage = async () => {
+
+        const data = await fetch('https://dog.ceo/api/breeds/image/random');
+
+        const message = await data.json();
+        console.log(message);
+        setMessage(message);
+    }
+
   return (
     <div>
-        <h1>Shop Page</h1>
+       {messages.map(message => (
+       <h1>{message.message}</h1>
+       ))}
+        
     </div>
   );
 }
