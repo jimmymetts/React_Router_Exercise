e96c90c685722152bd4873c1a1c8acd862a2e717
jimmymetts <jimmymetts@bellsouth.net>
2019-10-13 20:01:31 -0500

    add links to nav items

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import './App.css';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
-import { BrowserRouterProps as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <About />
-      <Shop />
-
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/Shop" component={Shop}/>
+          </Switch>
+      </div>
+    </Router>
+      
+    
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
+
 
 export default App;
